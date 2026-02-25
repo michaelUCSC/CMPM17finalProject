44956678f23657b76e013c4206c914154d75a9c6
Milkie2 <screamingllama111@gmail.com>
2026-02-24 18:55:15 -0800

    Writing in imports for processing

---
+++ BirdClassification.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
