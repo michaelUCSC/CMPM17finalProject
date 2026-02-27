88d88f2334c8a490f88f5c593a077311fb973b50
Milkie2 <screamingllama111@gmail.com>
2026-02-27 15:32:44 -0800

    Transforms added

--- BirdClassification.py
+++ BirdClassification.py
@@ -4,3 +4,25 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from PIL import Image
+import splitfolders
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import v2
+import os
+
+root = "IndianBird_Dataset/training_set/training_set"
+
+transforms = v2.Compose([
+    v2.ToTensor(),
+    v2.RandomResize(40,100),
+    v2.RandomHorizontalFlip([0.5]),
+    v2.RandomVerticalFlip([0.5]),
+    v2.RandomPerspective([0.5]),
+    v2.RandomChannelPermutation(),
+    v2.RandomInvert(0.1)
+])
+
+splitfolders.ratio(root,outputs="IndianBird_Dataset/training_set/training_set",
+                   ratio = (0.7,0.15,0.15))
+
+data = ImageFolder(root)
