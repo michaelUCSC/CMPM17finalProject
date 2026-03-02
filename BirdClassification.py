2acac6a3ba260a17b06dbe5979c500a03b3633cd
michaelUCSC <mtopuchy@ucsc.edu>
2026-03-01 22:55:57 -0800

    image display code (only use this for the image display code, everything else doesn't work for some reason)

--- BirdClassification.py
+++ BirdClassification.py
@@ -11,34 +11,59 @@ from torchvision.transforms import v2
 import os
 root = "IndianBird_Dataset/training_set/training_set"
 
-#Train/Test/Val Split:
+# #Transforms:
+# transforms = v2.Compose([
+#     v2.ToTensor(),
+#     v2.RandomResize(40,100),
+#     v2.RandomHorizontalFlip([0.5]),
+#     v2.RandomVerticalFlip([0.5]),
+#     v2.RandomPerspective([0.5]),
+#     v2.RandomChannelPermutation(),
+#     v2.RandomInvert(0.1)
+# ])
 
-#Dataloaders for Train, Val, Test
+# #Train/Test/Val Split:
+# splitfolders.ratio(root,outputs="IndianBird_Dataset/training_set/training_set",
+#                    ratio = (0.7,0.15,0.15))
 
-#Transforms:
-transforms = v2.Compose([
-    v2.ToTensor(),
-    v2.RandomResize(40,100),
-    v2.RandomHorizontalFlip([0.5]),
-    v2.RandomVerticalFlip([0.5]),
-    v2.RandomPerspective([0.5]),
-    v2.RandomChannelPermutation(),
-    v2.RandomInvert(0.1)
-])
+# #Dataloaders for Train, Val, Test
+# train_data = ImageFolder(os.path.join(root,'train'),transforms=transforms)
+# test_data = ImageFolder(os.path.join(root,'test'),transforms=transforms)
+# val_data = ImageFolder(os.path.join(root,'val'),transforms=transforms)
 
-splitfolders.ratio(root,outputs="IndianBird_Dataset/training_set/training_set",
-                   ratio = (0.7,0.15,0.15))
+# train_loader = DataLoader(train_data,batch_size=30,shuffle=True)
+# test_loader = DataLoader(test_data,batch_size=15,shuffle=False)
+# val_loader = DataLoader(val_data,batch_size=15,shuffle=False)
 
-data = ImageFolder(root)
+
+# data = ImageFolder(root)
 
 #Image Displays
 import os
+from PIL import Image
+import random
 
-p = "Users\\Public\\Documents"
+base_dir = os.path.dirname(__file__)
+p = os.path.abspath(os.path.join(base_dir, "..", "training_set"))
 
-for root, dirs, files in os.walk(p):
-    for n in files:
-        fp = os.path.join(root, n)
-        with open(fp, "r", errors="ignore") as f:
-            print(n)
-            print(f.read())
+valid_ext = {".jpg", ".jpeg", ".png", ".bmp", ".webp"}
+image_paths = []
+for root, _, files in os.walk(p):
+    for filename in files:
+        ext = os.path.splitext(filename)[1].lower()
+        if ext in valid_ext:
+            image_paths.append(os.path.join(root, filename))
+
+num_to_show = min(100, len(image_paths))
+random_images = random.sample(image_paths, k=num_to_show)
+
+plt.figure(figsize=(20, 5))
+for idx, fp in enumerate(random_images, start=1):
+    img = Image.open(fp)
+    plt1 = plt.subplot(5, 20, idx)
+    plt1.imshow(img)
+    plt1.set_title(os.path.basename(os.path.dirname(fp)))
+    plt1.axis('off')
+
+plt.tight_layout()
+plt.show()
