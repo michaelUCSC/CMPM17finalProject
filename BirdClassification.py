a7dff3039500984ca365a6576efa52de94146ca9
Milkie2 <screamingllama111@gmail.com>
2026-03-15 19:37:21 -0700

    Quick fix

--- BirdClassification.py
+++ BirdClassification.py
@@ -91,9 +91,9 @@ class ConvNet(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3,4,3,1,1)
         self.conv2 = nn.Conv2d(4,3,3,1,1)
-        self.conv3 = nn.Conv2d(3,48,3,1,1)
+        self.conv3 = nn.Conv2d(3,12,3,1,1)
         self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(12*12*48,50)
+        self.fc1 = nn.Linear(12*12*12,50)
         self.fc2 = nn.Linear(50,25)
         self.relu = nn.ReLU()
     def forward(self,x):
